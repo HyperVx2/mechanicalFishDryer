9faa33a2ff43e4c3b0eb94f46a1a1e8a0f3ead6c
Chad Aga Abinguna <chad.abinguna@gmail.com>
2024-05-26 01:22:34 +0800

    Set weight calibration value (229.63) and cleaned HX711 code.

--- src/main.ino
+++ src/main.ino
@@ -2,10 +2,10 @@
     Usable ESP32 pins (in order): 14, 27, 26, 25, 33, 32
     Ref: https://lastminuteengineers.com/esp32-pinout-reference
 
-    RELAY, DOUT, SCK, DHT
+    Assigned pin (in order): RELAY, HX711_DOUT, HX711_SCK, DHT
 
-    Ref 1 (dht22): 
-    Ref 2 (hx711): 
+    Ref 1 (dht22): https://github.com/adafruit/DHT-sensor-library/blob/master/examples/DHTtester/DHTtester.ino
+    Ref 2 (hx711): https://github.com/olkal/HX711_ADC/blob/master/examples/Read_1x_load_cell_interrupt_driven/Read_1x_load_cell_interrupt_driven.ino
     Ref 3 (scheduler): https://www.norwegiancreations.com/2017/09/arduino-tutorial-using-millis-instead-of-delay/
 */
 
@@ -14,7 +14,7 @@
 #include <EEPROM.h>
 
 /* scheduler code */
-int period = 1000; // in ms
+const int period = 1000; // in ms
 unsigned long time_now = 0;
 
 /* DHT22 code */
@@ -32,31 +32,25 @@ const int calVal_eepromAddress = 0;
 unsigned long t = 0;
 volatile boolean newDataReady;
 
-float weightCalValue = 0; // todo: change value
+float weightCalValue = 229.63; // todo: change value
 float weight = 0; // weight data
-
-// interrupt routune for HX711
-void dataReadyISR() {
-    if (loadCell.update()) {
-        newDataReady = 1;
-    }
-}
 
 void setup() {
     Serial.begin(9600); delay(10);
     Serial.println(); Serial.println("Starting...");
 
+    EEPROM.begin(512); // fetch the calibration value from eeprom
+    unsigned long stabilizingTime = 2000;
+    boolean _tare = true;  //set this to false if you don't want tare to be performed in the next step
+
     dht.begin();
     loadCell.begin();
 
+    loadCell.start(stabilizingTime, _tare);
     if (isnan(hum) || isnan(tem)) {
         Serial.println(F("Timeout, check DHT22 wiring and pin designations."));
         while (1);
     }
-
-    EEPROM.begin(512); // fetch the calibration value from eeprom
-    unsigned long stabilizeTime = 2000;
-    boolean _tare = true;  //set this to false if you don't want tare to be performed in the next step
     if (loadCell.getTareTimeoutFlag()) {
         Serial.println("Timeout, check MCU>HX711 wiring and pin designations.");
         while (1);
@@ -68,14 +62,22 @@ void setup() {
     attachInterrupt(digitalPinToInterrupt(HX711_dout), dataReadyISR, FALLING);
 }
 
+/* interrupt routune for HX711 */
+void dataReadyISR() {
+    if (loadCell.update()) {
+        newDataReady = 1;
+    }
+}
+
 void loop() {
     hum = dht.readHumidity();
     tem = dht.readTemperature();
+    
     if (newDataReady) {
         weight = loadCell.getData();
         newDataReady = 0;
     }
-    
+
     if(millis() >= time_now + period){
         Serial.print(F("humidity:"));
         Serial.print(hum);
@@ -85,7 +87,7 @@ void loop() {
         Serial.print(", weight:");
         Serial.println(weight);
         
-        time_now += period;
+        time_now = millis();
     }
 
     // receive comand from serial terminal, send 't' to initiate tare operation:
